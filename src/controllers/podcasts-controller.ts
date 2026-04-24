712f3b7fb5c1d51c098f97762288afbd15c3acc9
João Silva <joaoteixeirasilva33@gmail.com>
2026-04-24 14:24:46 -0300

    feat: create layer util

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import { getPodcastService } from "../services/podcasts-service";
+import { ok } from "../utils/http-helper";
 
 export const getPodcast = async (req: Request, res: Response) => {
     const data = await getPodcastService();
-    res.status(200).json(data);
+    const response = await ok(data);
+
+    res.status(response.statusCode).json(response.body);
 }
