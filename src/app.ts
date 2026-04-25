a432ca80f0397c2c5d96060ec41d52fc5e876707
João Silva <joaoteixeirasilva33@gmail.com>
2026-04-24 22:57:28 -0300

    feat: adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express, { json } from "express";
 import router from "./routes/routes";
+import cors from "cors";
 
 export const createApp = () => {
     const app = express();
 
     app.use(json());
+    app.use(cors());
+
+    app.use(cors({
+        origin: "*",
+        methods: ["GET", "POST", "PATCH", "DELETE"]
+    }));
 
     app.use("/api", router);
 
